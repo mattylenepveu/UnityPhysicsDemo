97d96877588bae3e9e4f00dea651326f4e8afda7
Matthew Le Nepveu <matthew@lenepveu.com>
2018-03-08 23:39:05 +1100

    First commit. Managed to get first person mode working and jumping but the jumping is broken at the moment.

---
+++ Physics Demonstration/Assets/Scripts/CameraController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour 
+{
+	public float sensitivity = 5.0f;
+	public float smoothing = 2.0f;
+	public float initialHeight = 1.0f;
+	public float crouchHeight = 0.5f;
+
+	private GameObject character;
+	private Vector2 mouseLook;
+	private Vector2 smoothV;
+	private bool crouching;
+	private float height;
+
+	// Use this for initialization
+	void Awake() 
+	{
+		character = this.transform.parent.gameObject;
+		crouching = false;
+		height = initialHeight;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() 
+	{
+		// Mouse delta
+		var md = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+
+		md = Vector2.Scale (md, new Vector2(sensitivity * smoothing, sensitivity * smoothing));
+		smoothV.x = Mathf.Lerp (smoothV.x, md.x, 1.0f / smoothing);
+		smoothV.y = Mathf.Lerp (smoothV.y, md.y, 1.0f / smoothing);
+		mouseLook += smoothV;
+		mouseLook.y = Mathf.Clamp (mouseLook.y, -60.0f, 60.0f);
+
+		transform.localRotation = Quaternion.AngleAxis (-mouseLook.y, Vector3.right);
+		character.transform.localRotation = Quaternion.AngleAxis(mouseLook.x, character.transform.up);
+
+		/*float heightDiff = initialHeight - crouchHeight;
+
+		if (Input.GetKeyDown(KeyCode.LeftShift)) 
+		{
+			if (crouching) 
+			{
+				crouching = false;
+			} 
+			else 
+			{
+				crouching = true;
+			}
+		}
+
+		if (crouching) 
+		{
+			height = crouchHeight;
+			transform.position = new Vector3 (transform.position.x, transform.position.y - heightDiff, 
+				transform.position.z);
+		} 
+		else 
+		{
+			height = initialHeight;
+			transform.position = new Vector3 (transform.position.x, transform.position.y + heightDiff, 
+				transform.position.z);
+		}*/
+	}
+}
---
+++ Physics Demonstration/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+	public float speed = 1.0f;
+	public float jumpForce = 5.0f;
+	//public float height = 1.0f;
+	public CapsuleCollider col;
+	public LayerMask groundLayers;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Awake() 
+	{
+		Cursor.lockState = CursorLockMode.Locked;
+		rb = GetComponent<Rigidbody>();
+		col = GetComponent<CapsuleCollider>();
+
+		/*float scaleY = GetComponent<CameraController>().GetHeight();
+		transform.localScale = new Vector3(transform.localScale.x, scaleY, transform.localScale.z);*/
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate() 
+	{
+		float moveX = Input.GetAxis("Horizontal") * speed;
+		float moveZ = Input.GetAxis("Vertical") * speed;
+		moveX *= Time.deltaTime;
+		moveZ *= Time.deltaTime;
+
+		transform.Translate(moveX, 0, moveZ);
+
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+		}
+
+		if (Input.GetKeyDown (KeyCode.Escape)) 
+		{
+			Cursor.lockState = CursorLockMode.None;
+		}
+
+		/*float scaleY = GetComponent<CameraController>().GetHeight();
+		transform.localScale = new Vector3(transform.localScale.x, scaleY, transform.localScale.z);*/
+	}
+
+	private bool IsGrounded()
+	{
+		return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, 
+			col.bounds.min.y, col.bounds.center.z), col.radius * 0.9f, groundLayers);
+	}
+}
